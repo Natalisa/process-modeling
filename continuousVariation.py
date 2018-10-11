0eedf64689d5b6a5d836d86e3147ae86c9c3f3c0
Boris Bochkarev <Boris-Bochkaryov@yandex.ru>
2018-10-11 08:57:53 +0700

    (Промежуточно) Построение графика, исправление генерации

--- continuousVariation.py
+++ continuousVariation.py
@@ -1,6 +1,5 @@
 import math
 import random as r
-import matplotlib.pyplot as plt
 
 def fDistributionDensity(x):
     """
@@ -16,19 +15,47 @@ def fDistributionDensity(x):
     else:
         return 0
 
-def continuousVariation():
+def continuousVariation(x):
     """
     Генерация непрерывной случайной величины по графику плотности
 
     """
-    S1 = 1 - math.sqrt(3)/2
-    S2 = math.sqrt(3)/2
-    div = S1/S2
-    ran = r.random()
-    if ran > div:
-        print ("s2", fDistributionDensity(ran))
+    if x > math.pi / 6:
+        print ("s2", fDistributionDensity(x))
     else:
-        print ("s1", fDistributionDensity(ran))
-    print(ran)
+        print ("s1", fDistributionDensity(x))
 
-continuousVariation()
+def our_rand():
+    res = r.random()
+    return (res, res * (2 * math.sqrt(3) + math.pi / 6))
+
+N = 10 ** 2
+ZN = []
+for _ in range(N):
+    (x_0_1, x) = our_rand()
+    ZN.append((x_0_1, continuousVariation(x)))
+
+n = 20
+# промежутки
+H = dict()
+for i in range(n):
+    H.update({i : 0})
+
+# количество попаданий
+for x, _ in ZN:
+    ind = int(x * n)
+    if ind == 20:
+        ind -= 1
+    H.update({ind : H.get(ind) + 1})
+
+print(H)
+
+# вывод графиков для H
+import numpy as np
+import matplotlib.pyplot as plt
+data1 = list(H.values())
+locs = np.arange(1, len(data1)+1)
+width = 0.27
+bar1 = plt.bar(locs, data1, width=width)
+plt.xticks(locs + width*1.5, locs)
+plt.show()
